1a73ba90133b0f197a7a9224204321c3700245f3
Hemsas Riane <rayan.hemsas@gmail.com>
2023-07-30 06:57:40 +0200

    Completed assignment

--- index.js
+++ index.js
@@ -1,2 +1,24 @@
 
 // Write your code here
+let num1 = 31;
+let num2 = 2;
+let multiply =num1*num2;
+
+
+
+let random=Math.ceil(Math.random());
+
+
+console.log( random);
+
+let num3= 34;
+let num4 =5;
+let mod =num3 % num4;
+// console.log(mod);
+
+let max=Math.max("4","20");
+// console.log(max);
+
+
+
+
